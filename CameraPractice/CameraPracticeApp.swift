e9eb3be0ae01c8a83ee4e729330fc39309f4f036
Tessa Delsener <wildtessa.70@icloud.com>
2024-05-12 15:15:28 -0700

    Initial Commit

---
+++ CameraPractice/CameraPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPracticeApp.swift
+//  CameraPractice
+//
+//  Created by Tessa Delsener on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CameraPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
